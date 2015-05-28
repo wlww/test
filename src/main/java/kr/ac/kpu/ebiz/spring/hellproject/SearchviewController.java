bf0b9b4b730404f6bf1f8659f166260d844c3f7c
helloworld <helloworld@gmail.com>
2015-05-28 14:05:18 +0900

    SearchviewController, mapper

--- src/main/java/kr/ac/kpu/ebiz/spring/hellproject/SearchviewController.java
+++ src/main/java/kr/ac/kpu/ebiz/spring/hellproject/SearchviewController.java
@@ -1,7 +1,25 @@
 package kr.ac.kpu.ebiz.spring.hellproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by User on 2015-05-27.
  */
+@Controller
+@RequestMapping("/search")
 public class SearchviewController {
+
+    @Autowired
+    ActorRepository actorRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView world() {
+        ModelAndView mav = new ModelAndView("/searchview");
+        mav.addObject("actors", actorRepository.selectAll());
+        return mav;
+    }
 }
